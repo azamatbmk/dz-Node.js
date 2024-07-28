9f1a5bf7dcdd0be8e738354a6520e85bb1ee729c
Azamat Kudziev <azamat.kudziev@gmail.com>
2024-07-28 12:11:49 +0300

    Коммит с 3-calc-events

---
+++ 3-calc-events/index.js
@@ -0,0 +1,28 @@
+let firstNum = Number(process.argv[2]);
+let secondNum = Number(process.argv[3]);
+let operation = process.argv[4];
+let sign = process.argv[5]
+
+const EventEmmiter = require('events');
+const calcEmmiter = new EventEmmiter();
+
+calcEmmiter.on(operation, (firstNum, secondNum) => {
+    switch(operation) {
+        case 'add':
+            console.log(firstNum + secondNum);
+            break;
+        case 'mult':
+            console.log(firstNum * secondNum);
+            break;
+        case 'div':
+            console.log(firstNum / secondNum);
+            break;
+        case 'sub':
+            console.log(firstNum - secondNum)
+            break;
+        default:
+            throw new Error(' Передайте одно из значений: add, mult, div, sub')
+    }  
+})
+    
+calcEmmiter.emit(operation, firstNum, secondNum)
