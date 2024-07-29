b7a4795d242830d6ccf246edf5047d83d4409944
Azamat Kudziev <azamat.kudziev@gmail.com>
2024-07-29 17:28:56 +0300

    Задание с таймером

---
+++ 4-timer/timer.js
@@ -0,0 +1,20 @@
+let seconds = Number(process.argv[2]) * 1000;
+let minutes = Number(process.argv[3]) * 60000;
+let hours = Number(process.argv[4]) * 3600000;
+
+
+function setTimer(seconds, minutes, hours) {
+    let time = 0;
+    if(!minutes && !hours) {
+        time = seconds 
+    } else if (!minutes) {
+        time = seconds + hours
+    } else if(!hours) {
+        time = seconds + minutes
+    }
+    setTimeout(() => { 
+        console.log('Прошло ' + time + ' миллисекунд')
+    }, time)
+}
+
+setTimer(seconds, minutes, hours)
