5ee22529c9c11dfe99d41417a1f024721475279f
Azamat Kudziev <azamat.kudziev@gmail.com>
2024-10-16 21:59:44 +0300

    ДЗ  прогноз погоды с тремя городами

---
+++ 8-weather-extended/services/storage.service.js
@@ -0,0 +1,42 @@
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const TOKEN_DICTIONARY = {
+    token: 'token',
+    city: 'city',
+    city2: 'city2',
+    city3: 'city3'
+}
+
+const saveKeyValue = async (key, value) => {
+    let data = {};
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        data = JSON.parse(file);
+    }
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data))
+};
+
+const getKeyValue = async (key) => {
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+        return data[key];
+    }
+    return undefined;
+};
+
+const isExist = async (path) => {
+    try {
+        await promises.stat(path);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
